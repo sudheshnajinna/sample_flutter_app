5956d70f460cb34423d94be90f7131ce2e60f83e
Sudheshna Jinna <sudheshnareddy1720@gmail.com>
2023-12-22 08:36:33 -0500

    added floating images

--- go_moon/lib/grid_item.dart
+++ go_moon/lib/grid_item.dart
@@ -14,69 +14,92 @@ class _GridItemState extends State<GridItem> {
 
   @override
   Widget build(BuildContext context) {
+    // Preset colors for normal state
     List<Color> boxColors = [
       Color.fromARGB(131, 5, 73, 140),
       Color.fromARGB(139, 5, 73, 150),
       Color.fromARGB(112, 8, 43, 94),
       Color.fromARGB(145, 5, 48, 85),
       Color.fromARGB(122, 3, 18, 40),
-      // Add as many colors as you have images or as needed for your design
     ];
 
+    // Preset colors for hover state
+    List<Color> hoverColors = [
+      // Replace with actual hover colors if needed
+      Color.fromARGB(114, 168, 95, 233), // Example yellow color
+      // Add more colors as needed
+    ];
+
+    // Preset values
     List<String> values = [
       '10,000',
       '12,000',
       '15,000',
       '16,000',
       '17,000',
-      // Add as many values as you have images or as needed for your design
     ];
 
-    // Ensure you have five images named 'image1.png' through 'image5.png' in your 'assets/images' folder.
     return MouseRegion(
       onEnter: (_) => setState(() => _isHovering = true),
       onExit: (_) => setState(() => _isHovering = false),
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 300),
-        height: 120, // Fixed height for each grid item
-        margin: const EdgeInsets.symmetric(vertical: 10), // Space between grid items
+        height: 120,
+        margin: const EdgeInsets.all(10),
         decoration: BoxDecoration(
           color: _isHovering
-              ? boxColors[widget.index % boxColors.length].withOpacity(0.5)
+              ? hoverColors[widget.index % hoverColors.length].withOpacity(0.5)
               : boxColors[widget.index % boxColors.length].withOpacity(0.3),
           borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-    BoxShadow(
-      color: Color.fromARGB(121, 131, 45, 212).withOpacity(0.5), // Adjust the shadow color and opacity
-      spreadRadius: 1,
-      blurRadius: 6,
-      offset: Offset(0, 3), // changes position of shadow
-    ),
-  ],
+          boxShadow: _isHovering
+              ? [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                  BoxShadow(
+                    color: Color.fromARGB(147, 168, 2, 130).withOpacity(0.5),
+                    spreadRadius: 4,
+                    blurRadius: 8,
+                    offset: Offset(0, 5),
+                  ),
+                ]
+              : [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: 4,
+                    blurRadius: 8,
+                    offset: Offset(0, 10),
+                  ),
+                ],
           border: Border.all(
             color: _isHovering
-                ? Color.fromARGB(147, 168, 2, 130).withOpacity(0.9)
-                : Color.fromARGB(145, 160, 109, 243).withOpacity(0.7),
-            width: 3,
+                ? Color.fromARGB(106, 63, 2, 101).withOpacity(0.9)
+                : Color.fromARGB(84, 160, 109, 243).withOpacity(0.7),
+            width: 6,
           ),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/images/image${widget.index + 1}.png', // Make sure image assets are correctly named and placed
-              width: 60, // Set a fixed width for the images
-              height: 60, // Set a fixed height for the images
+            Transform.translate(
+              offset: _isHovering ? Offset(0, -10) : Offset(0, 0),
+              child: Image.asset(
+                'assets/images/image${widget.index + 1}.png',
+                width: 60,
+                height: 60,
+              ),
             ),
-            SizedBox(height: 8), // Spacing between the image and text
+            SizedBox(height: 8),
             Text(
-              values[widget.index % values.length], // Use the values list
+              values[widget.index % values.length],
               style: TextStyle(
-                color: Color.fromARGB(248, 99, 5, 188),
+                color: _isHovering ? Colors.black : Colors.white,
                 fontWeight: FontWeight.bold,
                 fontSize: 18,
                 fontFamily: 'Times New Roman',
-    
               ),
             ),
           ],
